94b173c9687378c8e97db53a54599fc03981b1dc
Robert Bisewski <contact@ibiscybernetics.com>
2018-09-04 17:34:13 -0500

    added a small tool to print out a list of logs, might be helpful to determine their overall size

---
+++ print-var-log.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import sys, getopt, getpass
+from fabric import Connection
+
+def main(argv):
+
+    usageInfo = ("print-var-log.py "
+                 "-d <destination hostname or IP> "
+                 "-p <port number> "
+                 "-u <user> "
+                 "-i </path/to/identity_file>")
+
+    destinationHost = ''
+    destinationPort = 22 
+    user = ''
+    identityFile = ''
+    
+    #
+    # input validation
+    #
+    
+    try:
+        opts, args = getopt.getopt(argv,"hd:p:u:i:",["host=","port=","user=","id="])
+    except getopt.GetoptError:
+        print(usageInfo)
+        exit(1)
+    
+    for opt, arg in opts:
+        if opt == "-h":
+            print(usageInfo)
+            exit()
+        elif opt in ("-d","--host"):
+            destinationHost = arg
+        elif opt in ("-p","--port"):
+            destinationPort = arg
+        elif opt in ("-u","--user"):
+            user = arg
+        elif opt in ("-i","--id"):
+            identityFile = arg
+    
+    if destinationHost == '':
+        print("Error: No host specified.")
+        exit(1)
+    
+    if destinationPort == 0 or not destinationPort.isdigit():
+        print("Error: Invalid port specified.")
+        exit(1)
+    
+    if user == '':
+        print("Error: No user specified.")
+        exit(1)
+
+    #
+    # if an identity is supplied, attempt to obtain the passphrase
+    #
+    identityPassphrase = getpass.getpass()
+    
+    #
+    # attempt to connect using the given parameters
+    #
+    
+    if identityFile == '' or identityPassphrase == '':
+        conn = Connection(host=destinationHost, port=destinationPort, user=user)
+    else:
+        conn = Connection(host=destinationHost, port=destinationPort, user=user,
+          connect_kwargs={'key_filename':identityFile,'passphrase':identityPassphrase})
+    
+    if conn is None:
+        print("Error: Unable to initialize Connection object.")
+        exit(1)
+    
+    print("The following connection was established: ", conn)
+
+    #
+    # print the contents of /var/log/
+    #
+
+    result = conn.run("ls -lh /var/log/")
+
+    #
+    # attempt to clean up and close the connection
+    #
+
+    if result.exited != 0 or result.ok != True:
+        print("Warning: The connection was improper or terminated prematurely.")
+
+    conn.close()
+
+#
+# execute the main function declared above
+#
+main(sys.argv[1:])
